0145995ce455a02985fd1404e43d51b688754f4b
MOHD ASIF ANSARI <mohdasifansair1999@gmail.com>
2024-04-28 15:42:01 +0530

    commit

---
+++ src/components/WeaponsHeavy.tsx
@@ -0,0 +1,238 @@
+import { Link } from "react-router-dom";
+import Arrow from "../assets/arrow.svg";
+import CARD1 from "../assets/HomeCard/weapon.svg";
+import mouseicon from "../assets/mouseicon.svg";
+import agentbody from "../assets/Agent/agentbody.png";
+import gun1 from "../assets/guns/heavy1.png";
+import gun2 from "../assets/guns/heavy2.png";
+import damage from "../assets/guns/damage.svg";
+import basegun from "../assets/guns/gun1.png"
+const WeaponsHeavy = () => {
+  return (
+    <div className="flex justify-center gap-10">
+      <div className="flex flex-col items-center gap-3 -ml-72">
+        <h1 className="text-xl text-white font-semibold mb-2.5">
+          Select Sidearms
+        </h1>
+        <div className="flex flex-col gap-1 items-center">
+          <div className="bg-[#1A1C20] w-[70px] h-[70px] rounded-full">
+            <img
+              src={gun1}
+              alt="logo"
+              className="w-[50px] h-[50px] object-contain top-0 bottom-0 left-0 right-0 m-auto mt-4"
+            />
+          </div>
+          <p className="text-[#B5B9BD] mb-4 text-lg">Ares</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <div className="bg-[#1A1C20] w-[70px] h-[70px] border-4 border-[#2E9BFA]  rounded-full">
+            <img
+              src={gun2}
+              alt="logo"
+              className="w-[50px] h-[50px] object-contain top-0 bottom-0 left-0 right-0 m-auto mt-4"
+            />
+          </div>
+          <p className="text-[#B5B9BD] mb-4 text-lg">Odin</p>
+        </div>
+      </div>
+      <div className="max-w-[1444px] w-full px-4">
+        <h3 className="flex items-center gap-2 text-xl text-gray-400 font-normal tracking-tighter mb-5">
+          <Link to="/home">
+            <img src={Arrow} alt="logo" className="w-[25px] h-[15px]" />
+          </Link>
+          <span>Game Guide / Valorant /</span>
+          <span className="text-white font-normal">Weapons</span>
+        </h3>
+        <div className="flex flex-col gap-2.5 p-7  bg-[#1A1C1F] h-[200px] md:h-[175px] rounded-t-2xl">
+          <div className="flex items-center gap-4 ">
+            <img
+              src={CARD1}
+              alt="logo"
+              className=" w-[40px] h-[40px] object-cover rounded-lg"
+            />
+            <span className="text-white text-2xl">Weapons</span>
+          </div>
+          <p className="text-white ">
+            Check out Classic headshot % by range, primary and alt fire modes
+            and rates of fire, magazine capacity, wall penetration rating
+          </p>
+          <div className="flex items-start gap-6 mt-4">
+            <Link to="/weapons">
+              <p className="text-xl text-[#7D8185]"> Sidearms</p>
+            </Link>
+            <Link to="/smgs">
+              <p className=" text-xl text-[#7D8185]">SMGs</p>
+            </Link>
+            <Link to="/shotguns">
+              <p className="text-xl text-[#7D8185]">Shotguns</p>
+            </Link>
+            <Link to="/rifles">
+              <p className="text-xl text-[#7D8185]">Rifles</p>
+            </Link>
+            <Link to="/snipers">
+              <p className="text-xl text-[#7D8185]">Snipers</p>
+            </Link>
+            <Link to="/heavy">
+              <p className="font-semibold text-2xl text-white border-b-4 pb-2 border-[#2E9BFA]">
+                Heavy
+              </p>
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex flex-col bg-[#0c0c0c] sm:h-[1600px] md:h-[1440px] p-10  rounded-b-2xl  gap-x-4 gap-y-4">
+          <div className="flex items-start gap-6 ">
+            <Link to="/home/details">
+              <p className="flex font-semibold text-2xl items-center text-white border-b-4 pb-2 border-[#2E9BFA]">
+                <img src={mouseicon} alt="logo" className="w-[32px] h-[32px]" />
+                <p>Primary</p>
+              </p>
+            </Link>
+            <Link to="/teamdeath" className="flex">
+              <img src={mouseicon} alt="logo" className="w-[32px] h-[32px]" />
+              <p className=" text-xl text-[#7D8185]">Alt. Fire</p>
+            </Link>
+          </div>
+          <div className="flex">
+            <div className="flex flex-col">
+              <img
+                src={agentbody}
+                alt="logo"
+                className="w-[800px] h-[500px] rounded-3xl p-2"
+              />
+              <div className="p-2 flex items-center gap-16">
+                <div className="flex items-center gap-4">
+                  <p className="text-xl text-[#7D8185]">Classic</p>
+                  <p className="text-white font-semibold text-xl">Primary</p>
+                </div>
+                <div className="flex items-center gap-4">
+                  <p className="text-xl text-[#7D8185]">Fire Mode</p>
+                  <p className="text-white font-semibold text-xl">
+                    Semi-Automatic
+                  </p>
+                </div>
+                <div className="flex items-center gap-4">
+                  <p className="text-xl text-[#7D8185]">Fire Rate</p>
+                  <p className="text-white font-semibold text-xl">
+                    6.57 <span className="text-[#7D8185]">Rounds/Sec</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col p-2">
+              <div className="bg-[#1A1C1F] w-[420px] h-[200px] rounded-3xl flex flex-col border-4 border-blue-400  z-30 ">
+                <img
+                  src={basegun}
+                  alt="logo"
+                  className="w-[130px] h-[90px] rounded-2xl top-0 bottom-0 left-0 right-0 m-auto mt-10"
+                />
+                <p className="text-white text-2xl text-center mb-10">Classic</p>
+              </div>
+              <div className="flex flex-col  h-[820px] w-[420px] rounded-b-3xl bg-[#1A1C1F] -mt-4">
+                <div className="flex flex-col gap-4 mt-8">
+                  <div className="flex flex-col gap-4 px-6 mt-5 text-2xl">
+                    <p className="text-white text-2xl ">Pricing</p>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Cost</p>
+                      <p className="text-white text-2xl font-semibold">150</p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-4 px-6 mt-5 text-2xl">
+                    <p className="text-white text-2xl ">Primary Fire</p>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Fire-Mode</p>
+                      <p className="text-white text-2xl font-semibold">
+                        Semi-Automatic
+                      </p>
+                    </div>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Rate of Fire</p>
+                      <p className="text-white text-2xl font-semibold">
+                        6.76<span className="text-[#7D8185]">Rounds/Sec</span>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-4 px-6 mt-5 text-2xl">
+                    <p className="text-white text-2xl ">Ail. Fire</p>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Fire Mode</p>
+                      <p className="text-white text-2xl font-semibold justify-end items-center">
+                        3-Round Burst, spread increase
+                      </p>
+                    </div>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Rate of Fire</p>
+                      <p className="text-white text-2xl font-semibold">
+                        2.22<span className="text-[#7D8185]">Rounds/Sec</span>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-4 px-6 mt-5 text-2xl">
+                    <p className="text-white text-2xl ">Other Details</p>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Magazine Capacity</p>
+                      <p className="text-white text-2xl font-semibold">12</p>
+                    </div>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Wall Penetration</p>
+                      <p className="text-white text-2xl font-semibold">Low</p>
+                    </div>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Reload Speed</p>
+                      <p className="text-white text-2xl font-semibold">Low</p>
+                    </div>
+                    <div className="flex justify-between">
+                      <p className="text-[#7D8185]">Drop off Distance</p>
+                      <p className="text-white text-2xl font-semibold">Low</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-[#1A1C1F] rounded-3xl h-[260px] w-[420px] mt-4 ">
+                <div className="flex justify-between p-5">
+                  <div className="flex flex-col justify-between">
+                    <p className="text-center mb-3 text-xl text-[#7D8185]">
+                      0-30
+                    </p>
+                    <div className="flex">
+                      <img
+                        src={damage}
+                        alt="logo"
+                        className="w-[90px] h-[170px]"
+                      />
+                      <div className="flex flex-col justify-between">
+                        <p className="text-[#2E9BFA] text-xl"> - 78</p>
+                        <p className="text-[#266AC6] text-xl mb-3"> - 32</p>
+                        <p className="text-[#B5B9BD] text-xl mb-2.5"> - 23</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col justify-between ">
+                    <p className="text-center mb-3 text-xl text-[#7D8185]">
+                      0-30
+                    </p>
+                    <div className="flex">
+                      <img
+                        src={damage}
+                        alt="logo"
+                        className="w-[90px] h-[170px]"
+                      />
+                      <div className="flex flex-col justify-between ">
+                        <p className="text-[#2E9BFA] text-xl"> - 78</p>
+                        <p className="text-[#266AC6] text-xl mb-3"> - 32</p>
+                        <p className="text-[#B5B9BD] text-xl mb-2.5"> - 23</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeaponsHeavy;
---
+++ src/components/maps/Split.tsx
@@ -0,0 +1,139 @@
+import { Link } from "react-router-dom";
+import agent1 from "../../assets/Agent/agent1.png";
+import agent2 from "../../assets/Agent/agent2.png";
+import agent3 from "../../assets/Agent/agent3.png";
+import agent4 from "../../assets/Agent/agent4.png";
+import agent5 from "../../assets/Agent/agent5.png";
+import agent6 from "../../assets/Agent/agent6.png";
+import map from "../../assets/map/map9.png";
+import Arrow from "../../assets/arrow.svg";
+import downarrow from "../../assets/downarrow.svg";
+const Split = () => {
+  return (
+    <div className="flex justify-between ">
+      <div className="flex flex-col items-center gap-3 ">
+        <h1 className="text-xl text-white w-52 text-center font-semibold mb-2.5">
+          Select Maps
+        </h1>
+        <div className="flex flex-col gap-1 items-center">
+          <img
+            src={agent1}
+            alt="logo"
+            className=" w-[55px] h-[55px] rounded-full object-contain "
+          />
+          <p className="text-[#B5B9BD] mb-4 text-lg">Ares</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <img
+            src={agent2}
+            alt="logo"
+            className="  w-[55px] h-[55px] rounded-full object-contain"
+          />
+          <p className="text-[#B5B9BD] mb-4 text-lg">Odin</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <img
+            src={agent3}
+            alt="logo"
+            className="  w-[55px] h-[55px] rounded-full object-contain"
+          />
+          <p className="text-[#B5B9BD] mb-4 text-lg">Odin</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <img
+            src={agent4}
+            alt="logo"
+            className="  w-[55px] h-[55px] rounded-full object-contain"
+          />
+          <p className="text-[#B5B9BD] mb-4 text-lg">Odin</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <img
+            src={agent5}
+            alt="logo"
+            className=" w-[55px] h-[55px] rounded-full object-contain border-4 border-[#2E9BFA]"
+          />
+          <p className="text-[#B5B9BD] mb-4 text-lg">Odin</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <img
+            src={agent6}
+            alt="logo"
+            className="  w-[55px] h-[55px] rounded-full object-contain"
+          />
+          <p className="text-[#B5B9BD] mb-4 text-lg">Odin</p>
+        </div>
+      </div>
+      <div className="flex flex-col w-[1000px]">
+        <div className="flex items-center gap-2 text-xl text-gray-400 font-normal tracking-tighter mb-5">
+          <Link to="/home/details">
+            <img src={Arrow} alt="logo" className="w-[25px] h-[15px]" />
+          </Link>
+          <span>Game Guide / Valorant / Maps / Standard /</span>
+          <span className="text-white font-normal">Split</span>
+        </div>
+
+        <div className="flex flex-col gap-8 mt-16">
+          <div className="flex items-center gap-3">
+            <h1 className="text-5xl text-white font-semibold">Split . </h1>
+            <span className="text-3xl text-white font-thin mt-2">
+              Valorant Map
+            </span>
+          </div>
+          <p className="text-2xl text-white font-thin">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+          </p>
+          <div className="flex gap-32 items-center">
+            <h1 className="text-2xl text-white font-bold ">Map Legends</h1>
+            <img src={downarrow} alt="logo" className="w-[25px] h-[15px]" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="flex gap-6 items-center">
+              <span className="w-[30px] h-[30px] bg-[#64998C] rounded-lg "></span>
+              <p className="text-2xl text-white font-thin">
+                Defenders buy zone
+              </p>
+            </div>
+            <div className="flex gap-6 items-center">
+              <span className="w-[30px] h-[30px] bg-[#9E7174] rounded-lg "></span>
+              <p className="text-2xl text-white font-thin">
+                Attackers buy zone
+              </p>
+            </div>
+            <div className="flex gap-6 items-center">
+              <span className="w-[30px] h-[30px] bg-[#BC8C7C] rounded-lg "></span>
+              <p className="text-2xl text-white font-thin">Spike plant zone</p>
+            </div>
+            <div className="flex gap-6 items-center">
+              <span className="w-[40px] h-[7px] bg-[#FE4755] rounded-lg "></span>
+              <p className="text-2xl text-white font-thin">
+                Defenders Spawn barriers
+              </p>
+            </div>
+            <div className="flex gap-6 items-center">
+              <span className="w-[40px] h-[10px] bg-[#1BE2B3] rounded-lg "></span>
+              <p className="text-2xl text-white font-thin">
+                Attackers Spawn barriers
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img
+        src={map}
+        alt="logo"
+        className="w-[890px] h-[650px]  mt-16 object-contain "
+      />
+      <div className="flex flex-col justify-between p-3 bg-[#212426] w-[50px] h-[100px] mt-96 rounded-xl">
+        <p className="text-3xl text-center font-semibold text-gray-600 hover:text-blue-600 hover:text-3xl bottom-0">
+          +
+        </p>
+        <p className="text-3xl text-center font-semibold text-gray-600 hover:text-blue-600 hover:text-3xl bottom-0">
+          -
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Split;
